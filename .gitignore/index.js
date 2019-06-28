880fe0e11d6897feffa1f94f5de4f737fbc11541
Cookiiie <52327873+Cookiiie@users.noreply.github.com>
2019-06-28 18:18:50 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+bot.on('ready', function () {
+    bot.user.setGame("CookieBot, !help");
+    console.log("Je suis connecté !")
+    });
+    bot.login('NTk0MTcwMjYzMzUyOTAxNjM1.XRYing.00T4p0c7yAXehj33vWU7YDPQVNU')
+
+    bot.on('message', message => {
+        if (message.content === 'cookie') {
+        message.channel.sendMessage('Eat me !');
+        }
+        })
+    bot.on('message', message => {
+         if (message.content === 'cookies') {
+        message.channel.sendMessage('Eat me !');
+        }
+        });
+
+   
