5b6705b6f02414a684b1ede822a997477eb8a27d
Cookiiie <52327873+Cookiiie@users.noreply.github.com>
2019-06-28 18:28:16 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ bot.on('ready', function () {
     bot.user.setGame("CookieBot, !help");
     console.log("Je suis connecté !")
     });
-    bot.login('process.env.NTk0MTcwMjYzMzUyOTAxNjM1.XRYing.00T4p0c7yAXehj33vWU7YDPQVNU')
+    bot.login('process.env.TOKEN)
 
     bot.on('message', message => {
         if (message.content === 'cookie') {
