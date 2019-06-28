59d66c908d22ce85e01443638fc8ecb21fc0d046
Cookiiie <52327873+Cookiiie@users.noreply.github.com>
2019-06-28 19:15:15 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 bot.on('ready', function () {
-    bot.user.setGame("CookieBot, !help");
+    bot.user.setGame("CookieBot");
     console.log("Je suis connecté !")
     });
-    bot.login('process.env.TOKEN)
+    bot.login('process.env.TOKEN')
 
     bot.on('message', message => {
         if (message.content === 'cookie') {
